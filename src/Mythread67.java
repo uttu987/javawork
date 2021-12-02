5f41c9e7792d4d7dc6994b598bb00cc3594c6e9b
Utkarsh <utkarsh.agarwal_cs20@gla.ac.in>
2021-12-02 23:58:05 +0530

    fifth commit

---
+++ src/Mythread67.java
@@ -0,0 +1,19 @@
+public class Mythread67 extends Thread{
+    public void run()
+    {
+        for (int i=0;i<10;i++){
+            System.out.println("Mythread "+1);
+        }
+    }
+}
+class DemoException{
+    public static void main(String[] args) {
+        Mythread67 obj = new Mythread67();
+        obj.start();
+        for (int i=0; i<10;i++){
+            System.out.println("Main Thread"+1);
+        }
+    }
+
+
+}
